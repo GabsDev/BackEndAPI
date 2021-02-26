51a2b3fe3ec2ab266870d0f6610ab7b886ed07e1
Luis Avila <lavila@est.utn.ac.cr>
2021-02-25 18:45:47 -0600

    LineToSend

--- WebSocket/server.js
+++ WebSocket/server.js
@@ -2,7 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 
+const session = require("express-session");
+
 const lineToSendRoutes = require("./routes/lines-to-send_routes");
+const tasksRoutes = require("./routes/tasks_routes");
+const registrationsRoutes = require("./routes/registrations_routes");
+const sessionsRoutes = require("./routes/sessions_routes");
+const categoriesRoutes = require("./routes/categories_routes");
+
+const findUserMiddleware = require("./middlewares/find_user");
+const authUser = require("./middlewares/auth_user");
 
 const socketio = require("socket.io");
 
@@ -12,7 +21,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.set("view engine", "pug");
 
+app.use(
+  session({
+    secret: ["12iasdhasjdasdhbasjd", "123yhuasdgasdjkznxcjxzkh1y23"],
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+app.use(findUserMiddleware);
+app.use(authUser);
 app.use(lineToSendRoutes);
+app.use(tasksRoutes);
+app.use(registrationsRoutes);
+app.use(sessionsRoutes);
+app.use(categoriesRoutes);
 
 app.get("/", function (req, res) {
   res.render("home", {});
@@ -28,6 +51,3 @@ io.on("connection", function (socket) {
 
   socket.on("disconnect", function () {});
 });
-
-const client = require("./realtime/client");
-module.exports = app;
